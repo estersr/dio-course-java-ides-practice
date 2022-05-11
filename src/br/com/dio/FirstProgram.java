d9ebd335950e931a69a6625b46cd3b1cf3307482
estersr <estherramoos@gmail.com>
2022-05-11 18:17:11 -0300

    IntelliJ first commit.

---
+++ src/br/com/dio/FirstProgram.java
@@ -0,0 +1,22 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class FirstProgram {
+    public static void main(String[] args) {
+        Gato gato = new Gato();
+        System.out.println(gato);
+
+
+        // ctrl shift altera a linha de lugar
+        //ctrl y deletar linha, ctrl z recuperar o que apagou
+        // ctrl d para duplicar linha
+        // ctrl alt l para identar
+        // ctrl alt o apaga os imports nao utilizados
+        // int a = 5;
+        //int b = 3
+        //System.out.println("Hello World! " + (a+b));
+
+
+    }
+}
