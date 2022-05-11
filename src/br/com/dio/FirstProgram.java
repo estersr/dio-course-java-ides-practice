014506c347d8ad49b29626c52084dd3b1eedfa4e
estersr <estherramoos@gmail.com>
2022-05-11 18:31:15 -0300

    Creating Livro class.

--- src/br/com/dio/FirstProgram.java
+++ src/br/com/dio/FirstProgram.java
@@ -7,7 +7,14 @@ public class FirstProgram {
         Gato gato = new Gato();
         System.out.println(gato);
 
+        Livro livro1 = new Livro("the three bodies problem", 300);
+        System.out.println(livro1);
 
+
+        //ctrl k abrir area do commit
+        // shft ctrl f10 atualizar td, shft f10 so atualizar
+        // alt enter criar variavel
+        // alt insert generate
         // ctrl shift altera a linha de lugar
         //ctrl y deletar linha, ctrl z recuperar o que apagou
         // ctrl d para duplicar linha
@@ -18,5 +25,39 @@ public class FirstProgram {
         //System.out.println("Hello World! " + (a+b));
 
 
+    }
+}
+
+class Livro {
+    private String nome;
+    private Integer nPagina;
+
+    public Livro(String nome, Integer nPagina) {
+        this.nome = nome;
+        this.nPagina = nPagina;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getnPagina() {
+        return nPagina;
+    }
+
+    public void setnPagina(Integer nPagina) {
+        this.nPagina = nPagina;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", nPagina=" + nPagina +
+                '}';
     }
 }
